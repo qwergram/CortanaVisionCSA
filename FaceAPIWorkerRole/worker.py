7f7e05a31d614636f3f7e0ca4c68fff78d7f32a1
Norton Pengra <npengra317@gmail.com>
2016-08-22 15:27:38 -0700

    Clean dead code

--- FaceAPIWorkerRole/worker.py
+++ FaceAPIWorkerRole/worker.py
@@ -1,45 +1,11 @@
 ﻿import json
 from time import sleep
 
-#
-# The azure library provides access to services made available by the
-# Microsoft Azure platform, such as storage and messaging. 
-#
-# See http://go.microsoft.com/fwlink/?linkid=254360 for documentation and
-# example code.
-#
 from azure.storage.blob import BlockBlobService
 from azure.storage.queue import QueueService
-from secrets import ACCOUNT_NAME
-from secrets import ACCOUNT_KEY
 
-#
-# The CloudStorageAccount provides factory methods for the queue, table, and
-# blob services.
-#
-# See http://go.microsoft.com/fwlink/?linkid=246933 for Storage documentation.
-#
+from secrets import ACCOUNT_KEY, ACCOUNT_NAME
 
-# if os.environ.get('EMULATED', '').lower() == 'true':
-#     # Running in the emulator, so use the development storage account
-#     storage_account = CloudStorageAccount(None, None)
-# else:
-#     storage_account = CloudStorageAccount(STORAGE_ACCOUNT_NAME, STORAGE_ACCOUNT_KEY)
-
-# BLOB_SERVICE = storage_account.create_blob_service()
-# table_service = storage_account.create_table_service()
-# queue_service = storage_account.create_queue_service()
-
-#
-# Service Bus is a messaging solution for applications. It sits between
-# components of your applications and enables them to exchange messages in a
-# loosely coupled way for improved scale and resiliency.
-#
-# See http://go.microsoft.com/fwlink/?linkid=246934 for Service Bus documentation.
-#
-# SERVICE_BUS_NAMESPACE = '__paste_your_service_bus_namespace_here__'
-# SERVICE_BUS_KEY = '__paste_your_service_bus_key_here__'
-# bus_service = ServiceBusService(SERVICE_BUS_NAMESPACE, SERVICE_BUS_KEY, issuer='owner')
 
 class ImageQueue(object):
 
@@ -72,7 +38,7 @@ class ImageQueue(object):
         if len(self.dequeued) > self.max_unsorted_allowed:
             self.delete_last_images()
         self.dequeued.append(message.content)
-        
+
         return message.content
 
     def delete_last_images(self):
@@ -82,6 +48,8 @@ class ImageQueue(object):
 
     def __len__(self):
         return self.queue.get_queue_metadata(self.queue_name).approximate_message_count
+
+
 
 
 if __name__ == '__main__':
@@ -101,4 +69,3 @@ if __name__ == '__main__':
         # to avoid consuming 100% CPU time while your worker has no work.
         #
         sleep(1.0)
-
