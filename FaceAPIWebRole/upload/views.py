22c75c04b1722a069a84e56ae850b6d132f95c6d
Norton Pengra <npengra317@gmail.com>
2016-08-22 11:34:06 -0700

    Include new VIew

--- FaceAPIWebRole/upload/views.py
+++ FaceAPIWebRole/upload/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 # Create your views here.
+
+class IndexUploadView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(IndexUploadView, self).get_context_data(*args, **kwargs)
+        # context['something'] = model.objects.all()[:int]
+        return context
