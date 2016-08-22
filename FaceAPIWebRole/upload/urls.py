00f47f48bd0052d689e875f764354407e548f734
Norton Pengra <npengra317@gmail.com>
2016-08-22 11:34:16 -0700

    update urls to reflect views

--- FaceAPIWebRole/upload/urls.py
+++ FaceAPIWebRole/upload/urls.py
@@ -3,7 +3,8 @@ Definition of urls for FaceAPIWebRole.
 """
 
 from django.conf.urls import include, url
+from upload.views import IndexUploadView
 
 urlpatterns = [
-    
+    url(r'^$', IndexUploadView.as_view()),
 ]
