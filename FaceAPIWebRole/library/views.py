12864a56db4054486d14ac1a44e5b27999b2b104
Norton Pengra <npengra317@gmail.com>
2016-08-22 17:54:07 -0700

    define context for each template view

--- FaceAPIWebRole/library/views.py
+++ FaceAPIWebRole/library/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
+from django.views.generic import TemplateView
+from secrets import ACCOUNT_KEY, ACCOUNT_NAME
+from azure.storage.blob import BlockBlobService
 
 # Create your views here.
+
+
+def get_urls_from_container(containername):
+    blob = BlockBlobService(account_name=ACCOUNT_NAME, account_key=ACCOUNT_KEY)
+    return ["https://{}.blob.core.windows.net/{}/{}".format(ACCOUNT_NAME, containername, x.name) for x in blob.list_blobs(containername)]
 
 
 class UnsortedImageView(TemplateView):
@@ -9,6 +16,7 @@ class UnsortedImageView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(UnsortedImageView, self).get_context_data(*args, **kwargs)
+        context['image_list'] = get_urls_from_container('unsorted-images')
         return context
 
 
@@ -17,6 +25,7 @@ class FaceImageView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(FaceImageView, self).get_context_data(*args, **kwargs)
+        context['image_list'] = get_urls_from_container('has-face-images')
         return context
 
 
@@ -25,4 +34,5 @@ class NoFaceImageView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(NoFaceImageView, self).get_context_data(*args, **kwargs)
+        context['image_list'] = get_urls_from_container('no-face-images')
         return context
