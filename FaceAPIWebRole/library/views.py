f0a8126a227453f08eb44c991cf06c39cb2a8209
Norton Pengra <npengra317@gmail.com>
2016-08-22 17:40:45 -0700

    new views.py for library

--- FaceAPIWebRole/library/views.py
+++ FaceAPIWebRole/library/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 # Create your views here.
+
+
+class UnsortedImageView(TemplateView):
+    template_name = "folder/name.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(UnsortedImageView, self).get_context_data(*args, **kwargs)
+        return context
+
+
+class FaceImageView(TemplateView):
+    template_name = "samefolder"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(FaceImageView, self).get_context_data(*args, **kwargs)
+        return context
+
+
+class NoFaceImageView(TemplateView):
+    template_name = "samefolder"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(NoFaceImageView, self).get_context_data(*args, **kwargs)
+        return context
